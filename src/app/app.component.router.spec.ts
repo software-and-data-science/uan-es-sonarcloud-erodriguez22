70d6aa55fb424b2e267a64454f13de6a239c7b9d
Edwin Alejandro Rodriguez Beltran <63760625+AlejoRod83@users.noreply.github.com>
2020-04-23 21:26:58 -0500

    Update app.component.router.spec.ts

--- src/app/app.component.router.spec.ts
+++ src/app/app.component.router.spec.ts
@@ -136,7 +136,7 @@ function createComponent() {
   comp = fixture.componentInstance;
 
   const injector = fixture.debugElement.injector;
-  location = injector.get(Location) as SpyLocation;
+  location = injector.get(Location);
   router = injector.get(Router);
   router.initialNavigation();
   spyOn(injector.get(TwainService), 'getQuote')
