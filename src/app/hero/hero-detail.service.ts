2bd101bd34dabe9cf7ccc08b982f500513a19bb6
Edwin Alejandro Rodriguez Beltran <63760625+AlejoRod83@users.noreply.github.com>
2020-04-27 08:16:41 -0500

    Update hero-detail.service.ts

--- src/app/hero/hero-detail.service.ts
+++ src/app/hero/hero-detail.service.ts
@@ -10,7 +10,7 @@ export class HeroDetailService {
   // Returns a clone which caller may modify safely
   getHero(id: number | string): Promise<Hero> {
     if (typeof id === 'string') {
-      id = parseInt(id as string, 10);
+      id = parseInt(10);  // (id as string,10)
     }
     return this.heroService.getHero(id).then(hero => {
       return hero ? Object.assign({}, hero) : null; // clone or null
